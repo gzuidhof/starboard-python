b8c46b742661b87670ecd154b21b05ac30d02a28
Guido Zuidhof <guido@starboard.gg>
2021-04-30 02:43:16 +0100

    Another attempt at shipping types (0.6.2)

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import dts from "rollup-plugin-dts";
 
-
 const CleanCSS = require('clean-css');
 
 // Inline plugin to load css as minified string
@@ -18,7 +17,7 @@ const css = () => {return {
 }}
 
 
-export default {
+export default [{
   input: `src/index.ts`,
   output: [
     {file: 'dist/index.js', format: 'es'}
@@ -29,10 +28,18 @@ export default {
           './src/*.ts',
       ],
     }),
-    dts(),
     resolve(),
     commonjs(),
     css(),
   ]
+},
+{
+  input: `src/index.ts`,
+  output: [
+    {file: 'dist/index.d.ts', format: 'es'},
+  ], plugins: [
+    dts(),
+  ]
 }
+]
 ;
